77ffce9de69ed3d1707372e093c195eddd086954
Viktor Farcic <viktor@farcic.com>
2021-07-08 00:42:33 +0200

    Team A apps

--- config-cluster-aws.sh
+++ config-cluster-aws.sh
@@ -1,15 +1,19 @@
 export TEAM_NAME=$1
 
-export KUBECONFIG=$PWD/kubeconfig.yaml
-
-mkdir $TEAM_NAME-app-reqs
-
-cp -R team-app-reqs $TEAM_NAME-app-reqs
-
 export CLUSTER_NAME=$(\
     kubectl get clusters \
     --selector crossplane.io/composite=$TEAM_NAME \
     --output jsonpath="{.items[0].metadata.name}")
+
+export KUBECONFIG=$PWD/kubeconfig.yaml
+
+mkdir -p $TEAM_NAME-apps
+
+touch $TEAM_NAME-apps/dummy
+
+mkdir -p $TEAM_NAME-app-reqs
+
+cp -R team-app-reqs $TEAM_NAME-app-reqs
 
 aws eks --region us-east-1 \
     update-kubeconfig \
@@ -32,10 +36,6 @@ cat orig/team-app-reqs.yaml \
 cat orig/team-apps.yaml \
     | sed -e "s@server: .*@server: $SERVER_URL@g" \
     | tee production/$TEAM_NAME-apps.yaml
-
-mkdir $TEAM_NAME-apps
-
-touch $TEAM_NAME-apps/dummy
 
 git add .
 
